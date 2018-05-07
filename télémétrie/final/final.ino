e5f2054f53a06d6f23d89e4d621640985c75c317
Hugo <h2mille@gmail.com>
2018-05-07 14:23:01 +0200

    capteur interuption 40cm

--- télémétrie/final/final.ino
+++ télémétrie/final/final.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-#define SENSOR_NUMBER 4
+#define SENSOR_NUMBER 3
 
 #define NO_DETECT_THRETHOLD 5
 #define DETECT_THRETHOLD 3
@@ -20,45 +20,63 @@ uint8_t FIFO_sum;
 
 VL53L0X sensor[SENSOR_NUMBER];
 
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+ return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void setup() {
   uint8_t i;
+  Wire.setClock(400000);
   // put your setup code here, to run once:
   Serial.begin(115200);
-  capteur[0][0]=-200;
-  capteur[0][1]=200;
-  capteur[0][2]=PI/2;
+  Serial.println("boot");
+  capteur[0][0]=80;
+  capteur[0][1]=30;
+  capteur[0][2]=PI;
 
-  capteur[1][0]=200;
-  capteur[1][1]=200;
-  capteur[1][2]=PI/2;
+  capteur[1][0]=-80;
+  capteur[1][1]=30;
+  capteur[1][2]=PI;
  
-  capteur[2][0]=-200;
-  capteur[2][1]=-200;
-  capteur[2][2]=-PI/2;
+  capteur[2][0]=15;
+  capteur[2][1]=160;
+  capteur[2][2]=0;
 
-  capteur[3][0]=200;
-  capteur[3][1]=-200;
-  capteur[3][2]=-PI/2;
+  Serial.println("set i2c");
   Wire.begin();
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  digitalWrite(3,HIGH);
-  digitalWrite(4,LOW);
+
   digitalWrite(5,LOW);
   digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+
+
+
+
+  delay(100);
+  pinMode(8,INPUT);
+  delay(100);
   sensor[0].init();
-  sensor[0].setAddress(0X10);
-  digitalWrite(4,HIGH);
+  sensor[0].setAddress(12);
+  Serial.println("done.");
+  pinMode(7,INPUT);
+  delay(100);
   sensor[1].init();
-  sensor[1].setAddress(0X12);
-  digitalWrite(5,HIGH);
+  sensor[1].setAddress(10);
+  pinMode(6,INPUT);
+  delay(100);
   sensor[2].init();
-  sensor[2].setAddress(0X14);
-  digitalWrite(6,HIGH);
-  sensor[3].init();
-  sensor[3].setAddress(0X16);
+  sensor[2].setAddress(18);
+
   for(i=0;i<SENSOR_NUMBER;i++)
   {
     sensor[i].setTimeout(500);
@@ -69,6 +87,18 @@ void setup() {
     no_detect[i]=0;
     detect[i]=0;
   }
+  Serial.println("done.");
+  for(i=0;i<SENSOR_NUMBER;i++)
+  {
+    Serial.print("sensor: ");
+    Serial.println(i);
+    Serial.println(sensor[i].getAddress());
+    Serial.println(sensor[i].getSignalRateLimit());
+    Serial.println(sensor[i].readRangeSingleMillimeters());
+    sensor[i].startContinuous();
+  }
+  Serial.println("end setup");
+
 }
 
 
@@ -107,8 +137,8 @@ void parse_float(float* values,char* incomingBytes)
 }
 
 void add_coordinates(float* fisrt_coordinates,float* second_coordinates, float* coordinates_output){
-    coordinates_output[0]=fisrt_coordinates[0] - second_coordinates[0]*sin(fisrt_coordinates[2])+second_coordinates[0]*cos(fisrt_coordinates[2]);
-    coordinates_output[1]=fisrt_coordinates[1] + second_coordinates[1]*cos(fisrt_coordinates[2])+second_coordinates[1]*sin(fisrt_coordinates[2]);
+    coordinates_output[0]=fisrt_coordinates[0] - second_coordinates[0] * sin(fisrt_coordinates[2]) + second_coordinates[1] * cos(fisrt_coordinates[2]);
+    coordinates_output[1]=fisrt_coordinates[1] + second_coordinates[0] * cos(fisrt_coordinates[2]) + second_coordinates[1] * sin(fisrt_coordinates[2]);
     coordinates_output[2]=fisrt_coordinates[2] + second_coordinates[2];
 
 }
@@ -130,7 +160,9 @@ void add_fifo(){
   uint8_t i;
   int temp[SENSOR_NUMBER];
   for(i=0;i<SENSOR_NUMBER;i++)
-    temp[i]=sensor[i].readRangeContinuousMillimeters();
+  {
+      temp[i]=sensor[i].readRangeContinuousMillimeters();
+  }
   for(i=0;i<SENSOR_NUMBER;i++)
   {
     if(temp[i]>2500)
@@ -143,7 +175,7 @@ void add_fifo(){
       {
         FIFO_position[i]= (FIFO_position[i]+1)%10;
         FIFO[i][FIFO_position[i]]= 2500;
-        Serial.println("nothing detected for too long");
+//        Serial.println("nothing detected for too long");
         detect[i]=0;
       }
     }
@@ -188,6 +220,14 @@ void read_fifo(int32_t tab[]){
 
 }
 
+void print_coordinates(float* value)
+{
+     Serial.print(value[0]);
+     Serial.print(" ");
+     Serial.print(value[1]); 
+     Serial.print(" ");
+     Serial.println(value[2]); 
+}
 
 unsigned long time;
 void loop() {
@@ -198,33 +238,85 @@ void loop() {
   int temp;
   int32_t tab[3];
 
+
   add_fifo();
   read_fifo(tab);
+  capteur[2][2]=mapfloat(analogRead(A0),216,600,-0.7853,0.7853);
+  
+//    Serial.print(tab[0]);
+//    Serial.print(" ");
+//    Serial.print(tab[1]);
+//    Serial.print(" ");
+//    Serial.println(tab[2]);
+
   if (Serial.available() > 0)
   {
     time=millis();
-    Serial.print("Time: "); 
-    Serial.println(millis()-time);
+//    Serial.print("Time: "); 
+//    Serial.println(millis()-time);
+    uint8_t result_sum=0;
+    uint8_t result=0;
+    bool check;
+
+//    Serial.print("analog value: ");
+//    Serial.println(analogRead(A0));
 
     temp=quickReadByteUntil('\0', incomingBytes, sizeof(incomingBytes) / sizeof(char) );
     incomingBytes[temp]='\0';
     parse_float(coord,incomingBytes);
     for(temp=0;temp<SENSOR_NUMBER;temp++)
     {
+//      Serial.print("robot coordinates: ");
+//      print_coordinates(coord);
+
       add_coordinates(coord,capteur[temp],robot_coord[temp]);
+//      Serial.print("sensor coordinates coordinates: ");
+//      print_coordinates(robot_coord[temp]);
       coord_temp[0]=0;
       coord_temp[1]=tab[temp];
       coord_temp[2]=0;
-      add_coordinates(robot_coord[temp],coord_temp,robot_coord[temp]);
-      Serial.print("robot ");
-      Serial.println(temp);
-      Serial.println(robot_coord[temp][0]);
-      Serial.println(robot_coord[temp][1]);
-      Serial.println();
-    }
-    Serial.print("Time: ");
-    Serial.println(millis()-time);
-    Serial.println();
-  }
+       add_coordinates(robot_coord[temp],coord_temp,robot_coord[temp]);
+//      Serial.print("object coordinates: ");
+//      print_coordinates(robot_coord[temp]);
 
+      if((robot_coord[temp][0]>-1400) && (robot_coord[temp][0]<1400) && (robot_coord[temp][1]>100) && (robot_coord[temp][1]<1900))
+      {
+        result_sum++;
+        result |= 1<<temp;
+        check=true;
+      }
+      else
+        check=false;
+      if ((check == true) && ( tab[temp]<400))
+        Serial.print("1 ");
+      else
+        Serial.print("0 ");
+    }
+    
+    Serial.println(result_sum);
+    
+    if((result&1)!=0)
+    {
+        Serial.print(robot_coord[0][0]);
+        Serial.print(" ");
+        Serial.println(robot_coord[0][1]);
+    }
+    if((result&2)!=0)
+    {
+        Serial.print(robot_coord[1][0]);
+        Serial.print(" ");
+        Serial.println(robot_coord[1][1]);
+    }
+    if((result&4)!=0)
+    {
+        Serial.print(robot_coord[2][0]);
+        Serial.print(" ");
+        Serial.println(robot_coord[2][1]);
+    }
+
+//    Serial.print("Time: ");
+//    Serial.println(millis()-time);
+//    Serial.println();
+
+  }
 }
